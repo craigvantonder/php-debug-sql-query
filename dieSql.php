fc430b4559b7c29806324948945d8e5920769225
craigvantonder <craig@compulutions.co.za>
2016-08-26 07:50:00 +0200

    Initial commit

---
+++ dieSql.php
@@ -0,0 +1,16 @@
+<?php
+// Create helper function to fetch the error information from the mysqli database connection
+function dieSql($link)
+{
+  // Store the debug information
+  $debug = debug_backtrace();
+  // Die and dump the MySQL error information
+  echo '<h2>MySQL Error</h2>';
+  echo '<p><strong>Executing Script:</strong> '.$debug[0]['file'].'</p>';
+  echo '<p><strong>In Function:</strong> '.$debug[0]['function'].'</p>';
+  echo '<p><strong>On Line:</strong> '.$debug[0]['line'].'</p>';
+  echo '<p><strong>MySQL Error Number:</strong> '.$debug[0]['args'][0]->errno.'</p>';
+  echo '<p><strong>MySQL Error Information:</strong> '.$debug[0]['args'][0]->error.'</p>';
+  // Stop executing code
+  exit;
+}
